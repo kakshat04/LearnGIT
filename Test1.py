ba3c9e767adbe24b2682e7087445eb522fff935a
Kumar Akshat <kumar.akshat1988@gmail.com>
2020-03-20 20:18:32 +0530

    Initial Commit

---
+++ Test1.py
@@ -0,0 +1,26 @@
+def Test1(a):
+    # assert a<10 # if condition is true, return a, else return assertion4
+
+    if 0 <= a <= 10:
+        return "AAA"
+    elif a > 10:
+        return "BBB"
+    else:
+        # raise AssertionError("check number")
+        assert False, "CCC"
+    return a
+
+z = Test1(10)
+print(z)
+
+
+names = ["AB",
+         "B",
+         "C",]
+print(names)
+
+
+from Understanding_Underscores import *
+# print(internal1)
+z = A()
+print(z._internal())
