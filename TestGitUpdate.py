3bd6e6099aa2862875d2ed76e1602e8156f55e4d
Kumar Akshat <kumar.akshat1988@gmail.com>
2020-03-21 19:12:07 +0530

    Function changes

--- TestGitUpdate.py
+++ TestGitUpdate.py
@@ -16,4 +16,37 @@ def make_changes(func, text1):
 
 
 print(capital("abc"))
-print(makeChanges(capital, "Bxx"))
+print(make_changes(capital, "Bxx"))
+
+
+"""
+Function can be nested
+"""
+
+
+def test_nested(text):
+    def nested(t):
+        return t.upper()
+    return nested(text)
+
+
+def test_nested_number(inp):
+    if inp is "Mod":
+        def find_mod(num):
+            if num < 2:
+                return "Invalid Number, number should be greater than 2"
+            return num % 2
+        return find_mod
+    elif inp is "Div":
+        def find_div(num):
+            if num < 2:
+                return "Invalid Number, number should be greater than 2"
+            return num / 2
+        return find_div
+    else:
+        return "Input should be 'Mod' or 'Div'"
+
+
+print(test_nested("kumar akshat"))
+z = test_nested_number("Div")
+print(z(5))
